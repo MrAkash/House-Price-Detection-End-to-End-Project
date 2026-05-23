a592b9dca7cba6dea5aa84a6c58e660eec30c6e6
MrAkash <sky42161@gmail.com>
2026-05-23 19:15:22 +0530

    updated link

--- frontend/app.py
+++ frontend/app.py
@@ -10,9 +10,11 @@ age=st.number_input("Enter age")
 if st.button("Predict"):
     data={"area":area, "bedrooms":bedrooms, "age":age}
     response=requests.post(
-        "https://house-price-detection-end-to-end-project.onrender.com/",
+        "https://house-price-detection-end-to-end-project.onrender.com/predict",
         json=data
     )
     result=response.json()     # received prediction from fastapi 
-    st.success(f"Predicted Price:{result['predicted_price']}")
+    #st.write(result)           it use for debugging purpose to see that what the actual json is return
+    st.success(f"Predicted Price range:{result['predicted_price']}")
+
 
